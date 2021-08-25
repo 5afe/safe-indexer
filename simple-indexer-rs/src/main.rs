c32067876b5f5b510b8e6a3218d573eaf7449105
Jose Alvarez <jp.alvarezl@gmail.com>
2021-08-25 13:15:46 +0200

    Setup logging

--- simple-indexer-rs/src/main.rs
+++ simple-indexer-rs/src/main.rs
@@ -1,8 +1,11 @@
 extern crate celery;
 extern crate celery_codegen;
+extern crate log;
+extern crate dotenv;
 
 use celery::prelude::*;
 use anyhow::Result;
+use dotenv::dotenv;
 
 #[celery::task]
 fn add(x: i32, y: i32) -> TaskResult<i32> {
@@ -11,6 +14,9 @@ fn add(x: i32, y: i32) -> TaskResult<i32> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
     let my_app = celery::app!(
         broker = RedisBroker { std::env::var("REDIS_URI").unwrap_or_else(|_| "redis://127.0.0.1:6379".into()) },
         tasks = [add],
