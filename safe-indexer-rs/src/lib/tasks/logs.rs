76cb0a264c295a5ccf6c13a1ccd97bb774950958
Jose Alvarez <jp.alvarezl@gmail.com>
2021-08-27 13:57:23 +0200

    Added success and failure callback handlers

--- safe-indexer-rs/src/lib/tasks/logs.rs
+++ safe-indexer-rs/src/lib/tasks/logs.rs
@@ -1,9 +1,12 @@
 use crate::config;
 use celery::prelude::*;
-use ethcontract::prelude::*;
 use crate::models::json_rpc::{Request, Response};
 
-#[celery::task]
+// time_limit = 10 can be set to timeout the task
+#[celery::task(
+on_failure = incoming_eth_log_failure,
+on_success = incoming_eth_log_success,
+)]
 pub async fn check_incoming_eth(safe_address: String) -> TaskResult<Vec<String>> {
     let client = reqwest::Client::new();
     let request = Request::build_incoming_eth(&safe_address);
@@ -19,6 +22,20 @@ pub async fn check_incoming_eth(safe_address: String) -> TaskResult<Vec<String>>
     Ok(rpc_response.result.iter().map(|result| result.transaction_hash.to_string()).collect())
 }
 
+async fn incoming_eth_log_failure<T: Task>(task: &T, err: &TaskError) {
+    match err {
+        TaskError::TimeoutError => log::error!("Oops! Task {} timed out!", task.name()),
+        _ => log::error!("Task {} failed with {:?}", task.name(), err),
+    };
+}
+
+async fn incoming_eth_log_success<T: Task>(_task: &T, ret: &Vec<String>) {
+    let result: &Vec<String> = ret;
+    log::debug!("RETURN after type coercion {:#?}", result);
+}
+
+// use ethcontract::prelude::*;
+//
 // ethcontract-rs implementation
 // ethcontract::contract!(
 //     "abis/GnosisSafeL2.json",
