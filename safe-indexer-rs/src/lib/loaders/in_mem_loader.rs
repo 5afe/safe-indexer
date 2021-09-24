bd4c52832df4b448f2d48480508eebf21354a806
Jose Alvarez <jp.alvarezl@gmail.com>
2021-09-24 21:40:12 +0200

    Added an inmemory hashmap to mock a database

---
+++ safe-indexer-rs/src/lib/loaders/in_mem_loader.rs
@@ -0,0 +1,25 @@
+use tokio::sync::Mutex;
+use crate::rpc::models::RpcTransaction;
+use crate::loaders::EventLoader;
+use std::collections::HashMap;
+use crate::rpc::client::RpcClient;
+use async_trait::async_trait;
+
+pub struct InMemLoader {
+    rpc_client: RpcClient,
+    database: Mutex<HashMap<String, RpcTransaction>>,
+}
+
+#[async_trait]
+impl EventLoader for InMemLoader {
+    async fn was_tx_hash_checked(self, tx_hash: &str) -> bool {
+        self.database.lock().await.contains_key(tx_hash)
+    }
+
+    async fn process_tx_hash(self, tx_hash: &str) -> anyhow::Result<()> {
+        let transaction = self.rpc_client.get_transaction(tx_hash).await?;
+        let mut database = self.database.lock().await;
+        database.insert(tx_hash.to_string(), transaction);
+        Ok(())
+    }
+}
--- safe-indexer-rs/src/lib/loaders/mod.rs
+++ safe-indexer-rs/src/lib/loaders/mod.rs
@@ -1,2 +1,11 @@
-//TODO define an idempotent way of checking logs, tx_hash would be the idempotent id
-// pub struct EventLoader
+pub mod in_mem_loader;
+
+use async_trait::async_trait;
+
+//TODO there can be a few implementations: redis, postgres, in mem
+#[async_trait]
+pub trait EventLoader {
+    async fn was_tx_hash_checked(self, tx_hash: &str) -> bool;
+
+    async fn process_tx_hash(self, tx_hash: &str) -> anyhow::Result<()>;
+}
