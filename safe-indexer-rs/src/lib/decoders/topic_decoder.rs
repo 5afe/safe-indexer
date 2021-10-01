133dafbe3b43246de3c7a9b454dd879bdacce8a3
Jose Alvarez <jp.alvarezl@gmail.com>
2021-10-01 15:20:36 +0200

    Added a DecodedOutput type for topic decoder

--- safe-indexer-rs/src/lib/decoders/topic_decoder.rs
+++ safe-indexer-rs/src/lib/decoders/topic_decoder.rs
@@ -1,9 +1,33 @@
+use crate::decoders::EthDataDecoder;
+
 pub struct TopicDecoder;
 
-impl EthDataDecoder for TopicDecoder {}
+impl EthDataDecoder for TopicDecoder {
+    type DecodedOutput = TopicDecodedParams;
+
+    async fn decode(&self, chain_id: &str, data: &str) -> anyhow::Result<Self::DecodedOutput> {
+        todo!()
+    }
+
+    fn can_decode(&self, data: &str) -> bool {
+        !data.is_empty() && data.len() > 2
+    }
+}
 
 pub enum TopicDecodedParams {
-    ExecutionSuccess { pub tx_hash: String },
-    ExecutionFailure { pub tx_hash: String },
-    SafeMultisigTransaction,
+    ExecutionSuccess { tx_hash: String },
+    ExecutionFailure { tx_hash: String },
+    SafeMultisigTransaction {
+        to: String,
+        value: String,
+        data: String,
+        operation: String,
+        safe_tx_gas: String,
+        base_gas: String,
+        gas_price: String,
+        gas_token: String,
+        refund_receiver: String,
+        signatures: String,
+        additional_info: String,
+    },
 }
