8e3ec5e4deb7f7c1074871281ce0ea1b835aef27
Richard Meissner <msc.meissner@gmail.com>
2021-10-01 09:35:50 +0200

    Export constants

--- safe-indexer-ts/src/index.ts
+++ safe-indexer-ts/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./types";
 export * from "./indexer";
+// Ethers dependencies 
+// TODO: move to separate repo
 export * from "./ethers/loader";
 export * from "./ethers/parser";
+export * from "./ethers/constants";
