eabb1933209374b5029c7c12521e494efbeddd75
Jose Alvarez <jp.alvarezl@gmail.com>
2021-10-01 11:50:14 +0200

    Created elixir implementation

---
+++ safe-indexer-ex/lib/safe_indexer.ex
@@ -0,0 +1,18 @@
+defmodule SafeIndexer do
+  @moduledoc """
+  Documentation for `SafeIndexer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SafeIndexer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ safe-indexer-ex/test/safe_indexer_test.exs
@@ -0,0 +1,8 @@
+defmodule SafeIndexerTest do
+  use ExUnit.Case
+  doctest SafeIndexer
+
+  test "greets the world" do
+    assert SafeIndexer.hello() == :world
+  end
+end
