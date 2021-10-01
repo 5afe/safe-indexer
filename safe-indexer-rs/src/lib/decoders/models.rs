d87f7b32a72406d8b05221fb01d855a73869af1a
Jose Alvarez <jp.alvarezl@gmail.com>
2021-10-01 11:44:57 +0200

    Removed unused derived traits

--- safe-indexer-rs/src/lib/decoders/models.rs
+++ safe-indexer-rs/src/lib/decoders/models.rs
@@ -2,21 +2,21 @@ use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use crate::utils::json;
 
-#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone, Copy, Hash)]
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
 #[repr(u8)]
 pub enum Operation {
     CALL = 0,
     DELEGATE = 1,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct DataDecoded {
     pub method: String,
     pub parameters: Option<Vec<Parameter>>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Parameter {
     pub name: String,
@@ -29,20 +29,20 @@ pub struct Parameter {
     pub value_decoded: Option<ValueDecodedType>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
 pub enum ParamValue {
     SingleValue(String),
     ArrayValue(Vec<ParamValue>),
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
 pub enum ValueDecodedType {
     InternalTransaction(Vec<InternalTransaction>),
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct InternalTransaction {
     pub operation: Operation,
