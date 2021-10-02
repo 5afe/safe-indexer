ab2f295c39dee9dab6a27c666082fbf880fe3b8e
Jose Alvarez <jp.alvarezl@gmail.com>
2021-10-02 17:59:18 +0200

    Removed whitespace

--- safe-indexer-ex/lib/rpc_client/models.ex
+++ safe-indexer-ex/lib/rpc_client/models.ex
@@ -1,4 +1,4 @@
 defmodule RpcClient.Request do
     @derive Jason.Encoder
-    defstruct [  :method, jsonrpc: "2.0", params: [], id: "1"]
+    defstruct [:method, jsonrpc: "2.0", params: [], id: "1"]
 end 
